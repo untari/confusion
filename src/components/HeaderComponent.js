c7e7e4a5a632970ba0033f7158902c797b68961e
untari <sastraembun0306@gmail.com>
2020-06-02 10:17:16 +0700

    uncontrolled form

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem,  Jumbotron } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem,  Jumbotron, Button, Modal, ModalHeader,
+    ModalBody, Form, FormGroup, Input, Label } from 'reactstrap';
 import { NavLink} from 'react-router-dom';
 
 class Header extends Component {
     constructor(props) {
         super(props);
-    
-        this.toggleNav = this.toggleNav.bind(this);
+        
         this.state = {
-          isNavOpen: false
+          isNavOpen: false,
+          isModalOpen: false
         };
+        this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
       }
 
-      toggleNav() {
-        this.setState({
+      toggleNav() {this.setState({
           isNavOpen: !this.state.isNavOpen
         });
-      }
+        
+    }
+      
+      toggleModal() {
+           this.setState({
+                 isModalOpen: !this.state.isModalOpen
+           });
+    }
+    
+    handleLogin(event) {
+        this.toggleModal();
+        alert("Usarname: " + this.username.value + "Password: " + this.password.value 
+            + "Remember: " + this.remember.checked);
+        event.preventDefault();
+    }
+    
     render() {
         return(
             <div>
@@ -49,6 +67,13 @@ class Header extends Component {
                                             </NavLink>
                                         </NavItem>
                                         </Nav>
+                                        <Nav className="ml-auto" navbar>
+                                               <NavItem>
+                                                      <Button outline onClick={this.toggleModal}>
+                                                            <span className="fa fa-sign-in fa-lg"></span> Login
+                                                      </Button>
+                                               </NavItem>
+                                        </Nav>
                                  </Collapse>  
                            </div>
                     </Navbar>
@@ -63,6 +88,31 @@ class Header extends Component {
                                     </div>
                                 </div>
                     </Jumbotron>
+                    <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                             <ModalHeader toggle={this.toggleModel}>Login</ModalHeader>
+                             <ModalBody>
+                                    <Form onSubmit={this.handleLogin}>
+                                           <FormGroup>
+                                                 <Label htmlfor="username">Username</Label>
+                                                 <Input type="text" id="username" name="username"
+                                                     innerRef={(input) => this.username = input} />
+                                           </FormGroup>
+                                           <FormGroup>
+                                                 <Label htmlfor="password">Password</Label>
+                                                 <Input type="password" id="password" name="password" 
+                                                        innerRef={(input) => this.password = input} />
+                                           </FormGroup>
+                                           <FormGroup check>
+                                                 <Label check>
+                                                        <Input type="checkbox" name="remember" 
+                                                            innerRef={(input) => this.remember = input}/>
+                                                            Remember me
+                                                 </Label>
+                                           </FormGroup>
+                                           <Button type="submit" value="submit" color="bg-primary">Login</Button>
+                                    </Form>
+                             </ModalBody>
+                    </Modal>
              </div>
         );
     }
