79a043702e0a8dc6630454824e0e37bcef3ccf42
untari <sastraembun0306@gmail.com>
2020-06-07 07:52:23 +0700

    react animation cpmponent

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,158 +1,90 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem, Label, Button, Modal, ModalHeader, ModalBody, Row, Col } from 'reactstrap';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import React from 'react';
+import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import CommentForm from './CommentFormComponent';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
-
-const required = (val) => val && val.length;
-const maxLength = (len) => (val) => !(val) || (val.length <= len);
-const minLength = (len) => (val) => val && (val.length >= len);
-
-class CommentForm extends Component {
-
-  constructor(props) {
-    super(props);
-    this.toggleModal = this.toggleModal.bind(this);
-    this.state = {
-      isModalOpen: false
-    };
-  }
-
-  toggleModal() {
-    this.setState({
-      isModalOpen: !this.state.isModalOpen
-    });
-  }
-
-  handleSubmit(values) {
-    this.toggleModal();
-    this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <Button outline onClick={this.toggleModal}><span className="fa fa-edit fa-lg"></span> Submit Comment</Button>
-        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-          <ModalHeader>
-          Submit Comment
-          </ModalHeader>
-          <ModalBody>
-            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
-              <Row className="form-group">
-                <Label htmlFor="rating" md={12}>Rating</Label>
-                <Col md={12}>
-                  <Control.select model=".rating" className="form-control" name="rating">
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                  </Control.select>
-                </Col>
-              </Row>
-              <Row className="form-group">
-                <Label htmlFor="name" md={12}>Your Name</Label>
-                <Col md={12}>
-                  <Control.text model=".name" id="name" className="form-control" name="name" placeholder="Your Name"
-                                validators={{ required, minLength: minLength(3), maxLength: maxLength(15) }}/>
-                  <Errors className="text-danger" model=".name" show="touched"
-                          messages={{
-                            required: 'Required. ',
-                            minLength: 'Must be greater than 2 numbers. ',
-                            maxLength: 'Must be 15 numbers or less. '
-                          }} />
-                </Col>
-              </Row>
-              <Row className="form-group">
-                <Label htmlFor="comment" md={12}>Comment</Label>
-                <Col md={12}>
-                  <Control.textarea model=".message" id="message" rows="6" className="form-control" name="message" />
-                </Col>
-              </Row>
-              <Row className="form-group">
-                <Col>
-                  <Button type="submit" color="primary">
-                    Submit
-                  </Button>
-                </Col>
-              </Row>
-            </LocalForm>
-          </ModalBody>
-        </Modal>
-      </React.Fragment>
-    );
-  }
-
-}
-
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 function RenderDish( {dish} ) {
   return (
-      <div className="col-12 col-md-5 m-1">
-            <Card>
-                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
-     </div>
+    <FadeTransform in
+      transformProps={{
+        exitTransform: 'scale(0.5) translateY(-50%)'
+      }}>
+      <Card>
+        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    </FadeTransform>
   );
 }
 
-function RenderComments({comments, postComment, dishId}) {
+function RenderComments( {comments, dishId, postComment} ) {
 
-  if (comments == null) {
+  if (comments == null || comments.length === 0) {
     return (
       <div></div>
     );
   }
 
-  const renderedComments = comments.map((comment) => {
-    return (
-      <li key={comment.id}>
-        <p>{comment.comment}</p>
-        <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-      </li>
-    );
-  });
+  const renderedComments = (
+    <Stagger in>
+      {
+        comments.map((comment) => {
+          return (
+            <Fade in>
+              <li key={comment.id}>
+                <p>{comment.comment}</p>
+                <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+              </li>
+            </Fade>
+          );
+        })
+      }
+    </Stagger>
+  );
 
   return (
-     <React.Fragment>
-        <div>
-            <h4>Comments</h4>
-            <ul className="list-unstyled">
-            { renderedComments }
-            </ul>
-        </div>
-        <CommentForm dishId={dishId} postComment= {postComment} />
+    <React.Fragment>
+      <div>
+        <h4>Comments</h4>
+        <ul className="list-unstyled">
+          { renderedComments }
+        </ul>
+      </div>
+      <CommentForm dishId={dishId} postComment={postComment}/>
     </React.Fragment>
   );
 
 }
 
 const DishDetail = (props) => {
-   if (props.isLoading) {
-      return(
-         <div className="container">
-              <div className="row">
-                    <Loading />
-              </div>
-         </div>    
+
+  if (props.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
     );
-}
-else if (props.errMess) {
-    return(
-         <div className="container">
-              <div className="row">
-                    <h4>{props.errMess}</h4>
-              </div>
-         </div>    
+  }
+
+  else if (props.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <h4>{props.errMess}</h4>
+        </div>
+      </div>
     );
- }
- else if (props.dish != null) {
+  }
+
+  else if (props.dish != null) {
     return (
       <div className="container">
         <div className="row">
@@ -170,14 +102,12 @@ else if (props.errMess) {
             <RenderDish dish={props.dish} />
           </div>
           <div className="col-12 col-md-5 m-1">
-            <RenderComments comments={props.comments}
-               postomment={props.postComment}
-               dishId={props.dish.id} />
+            <RenderComments comments={props.comments} postComment={props.postComment} dishId={props.dish.id} />
           </div>
         </div>
-    </div>
-  );
- }
+      </div>
+    );
+  }
   else {
     return (
       <div></div>
