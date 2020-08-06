aeed630fe3832b009a724e7e07759f50be8ba3d9
untari <sastraembun0306@gmail.com>
2020-08-07 04:31:43 +0700

    Updates

---
+++ precache-manifest.dcc3a9dce05534a212eddcf5ae2aa12c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c4b4f5234786e85a4b9cc03fd623d32",
+    "url": "/confusion/index.html"
+  },
+  {
+    "revision": "af3974426be9df981e6a",
+    "url": "/confusion/static/css/2.05f68798.chunk.css"
+  },
+  {
+    "revision": "cf790e2ac465c357e166",
+    "url": "/confusion/static/css/main.8b72be6f.chunk.css"
+  },
+  {
+    "revision": "af3974426be9df981e6a",
+    "url": "/confusion/static/js/2.79123082.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/confusion/static/js/2.79123082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf790e2ac465c357e166",
+    "url": "/confusion/static/js/main.99ea80e9.chunk.js"
+  },
+  {
+    "revision": "22b0f3eaf86236a9a8f2",
+    "url": "/confusion/static/js/runtime-main.c372e2a2.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/confusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/confusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/confusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/confusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/confusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
